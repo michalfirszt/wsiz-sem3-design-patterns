fd6321e9602b345ffa9a454f0102952c86859398
michalfirszt <michalfiszu@gmail.com>
2023-10-14 08:39:04 +0200

    Initial commit

---
+++ 01/prototype.ts
@@ -0,0 +1,54 @@
+abstract class Vehicle {
+    constructor(newNumberOfWheels: number) {
+        this.numberOfWheels = newNumberOfWheels
+    }
+
+    private numberOfWheels: number = 0
+    private speed: string = '50km/h'
+    
+    /**
+     * getSpeed
+     */
+    public getSpeed() {
+        return this.speed
+    }
+
+    /**
+     * getNumberOfWheels
+     */
+    public getNumberOfWheels() {
+        return this.numberOfWheels
+    }
+
+    /**
+     * setNumberOfWheels
+     */
+    public setNumberOfWheels(value: number) {
+        this.numberOfWheels = value
+    }
+}
+
+class Car extends Vehicle {
+    constructor() {
+        super(4)
+    }
+
+    public engineName: string = ''
+
+    /**
+     * setEngineName
+     */
+    public setEngineName(newEngineName: string) {
+        this.engineName = newEngineName
+    }
+}
+
+let newCar = new Car()
+console.log(newCar.getNumberOfWheels())
+
+// let vehicleNumber1 = new Vehicle(2)
+// console.log(vehicleNumber1.getNumberOfWheels())
+
+// vehicleNumber1.setNumberOfWheels(4)
+// console.log(vehicleNumber1.getNumberOfWheels())
+
