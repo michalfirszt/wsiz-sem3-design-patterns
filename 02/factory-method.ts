501214a7d6fb6301ab826a2f70b6cb8b35b8e256
michalfirszt <michalfiszu@gmail.com>
2023-10-28 08:07:40 +0200

    factory method

--- 02/factory-method.ts
+++ 02/factory-method.ts
@@ -38,7 +38,37 @@ class Shop {
     }
 }
 
-const shop = new Shop()
+// const shop = new Shop()
 
-const bookNumber1 = shop.createBook()
-bookNumber1.order()
+// const bookNumber1 = shop.createBook()
+// bookNumber1.order()
+
+class Profile {
+    constructor(phoneNumberValue: number) {
+        this.phoneNumber = phoneNumberValue
+
+        console.log('user profile created')
+    }
+
+    public phoneNumber: number
+}
+
+class User {
+    constructor(name: string) {
+        this.username = name
+    }
+
+    public username: string
+
+    /**
+     * createProfile
+     */
+    public createProfile(userPhoneNumber: number) {
+        const userProfile = new Profile(userPhoneNumber)
+
+        return userProfile
+    }
+}
+
+const user1 = new User('Mike')
+user1.createProfile(123)
