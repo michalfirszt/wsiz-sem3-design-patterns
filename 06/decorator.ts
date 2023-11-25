9c11dc93c9b5d13d90a5c1f59630e8db9ea5a077
michalfirszt <michalfiszu@gmail.com>
2023-11-25 08:47:40 +0100

    Decorator example

---
+++ 06/decorator.ts
@@ -0,0 +1,48 @@
+class CarBase {
+    constructor() {}
+
+    /**
+     * openWindow
+     */
+    public openWindow() {
+        console.log('open base car window');
+    }
+
+    /**
+     * startEngine
+     */
+    public startEngine() {
+        console.log('start car engine');
+    }
+}
+
+class HybridCar extends CarBase {
+    constructor(car: CarBase) {
+        super()
+        this.baseCar = car
+    }
+
+    baseCar: CarBase;
+
+    /**
+     * startEngine
+     */
+    public startEngine() {
+        baseCar.startEngine()
+        console.log('start electric engine');
+    }
+}
+
+class HatchbackHybridCar extends HybridCar {
+    constructor(car: CarBase) {
+        super(car)
+    }
+}
+
+const baseCar = new CarBase()
+const hybridCar = new HybridCar(baseCar)
+
+hybridCar.startEngine()
+
+// #1
+// create decorator design pattern example
