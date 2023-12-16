0b3822ff540324168761254469e90af279ce41ab
michalfirszt <michalfiszu@gmail.com>
2023-12-16 09:07:22 +0100

    Observer example

---
+++ 07/observer.ts
@@ -0,0 +1,83 @@
+interface WeatherForecastSubject {
+    registerObserver: (observer: WeatherObserver) => void;
+    removeObserver: (observer: WeatherObserver) => void;
+    notifyObservers: () => void;
+}
+
+interface WeatherObserver {
+    updateTemerature: (temerature: number) => void;
+}
+
+class WeatherForecast implements WeatherForecastSubject {
+    constructor() {}
+
+    private observers: WeatherObserver[] = [];
+    private temerature: number = 0;
+
+    /**
+     * setTemperature
+     */
+    public setTemperature(newTemerature: number) {
+        this.temerature = newTemerature;
+
+        this.notifyObservers()
+    }
+
+    /**
+     * registerObserver
+     */
+    public registerObserver(observer: WeatherObserver) {
+        this.observers.push(observer)
+    }
+
+    /**
+     * removeObserver
+     */
+    public removeObserver(observer: WeatherObserver) {
+        this.observers.filter((currentObserver) => {
+            return currentObserver !== observer
+        })
+    }
+
+    /**
+     * notifyObservers
+     */
+    public notifyObservers() {
+        this.observers.forEach((currentObserver) => {
+            currentObserver.updateTemerature(this.temerature)
+        })
+    }
+}
+
+class WeatherWebsite implements WeatherObserver {
+    constructor(weatherForecast: WeatherForecast) {
+        weatherForecast.registerObserver(this)
+    }
+
+    /**
+     * updateTemerature
+     */
+    public updateTemerature(temerature: number) {
+        console.log(`WeatherWebsite update: new temerature: ${temerature}`);
+    }
+}
+
+class WeatherMobileApp implements WeatherObserver {
+    constructor(weatherForecast: WeatherForecast) {
+        weatherForecast.registerObserver(this)
+    }
+
+    /**
+     * updateTemerature
+     */
+    public updateTemerature(temerature: number) {
+        console.log(`new mobile app update:`);
+        console.log(`new temerature: ${temerature}`);
+    }
+}
+
+const weatherForecast = new WeatherForecast()
+const weatherWebsite = new WeatherWebsite(weatherForecast)
+const weatherMobileApp = new WeatherMobileApp(weatherForecast)
+
+weatherForecast.setTemperature(15)
